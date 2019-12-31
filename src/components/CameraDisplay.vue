d85f60862e2b8d817c7f6963f45c789f2667fe2e
jbull328 <jackbull328@gmail.com>
2019-12-31 11:46:54 -0800

    start branch

--- src/components/CameraDisplay.vue
+++ src/components/CameraDisplay.vue
@@ -1,7 +1,5 @@
 <template>
     <div class='ui container'>
-        <video v-if="!imageData.image" ref="video" class="camera-stream" />
-        <img v-else :src="imageData.image" v-bind:style="{transform: 'rotate(' + imageData.image_orientation + 'deg'}" class="camera-stream">
         <div class='ui divider'></div>
         <div class="icon-group">   
             <div  class="camera-icon" @click="captureImage">
@@ -22,8 +20,8 @@
             </div>
         </div>
     
-        <img v-if="!imageData.image" class="camera-stream" src="../../src/assets/image.png">
-        <img v-else :src="imageData.image" class="camera-stream" />
+        <img class="camera-stream" src="../../src/assets/image.png">
+        
     </div>
 
 
@@ -32,63 +30,8 @@
 
 <script>
     import axios from 'axios';
-    const API_IMAGE_ENDPOINT = 'https://ApiBackend.com/api/images';
 
-export default {
     
-
-    data() {
-        return {
-            defaultImage: '../../src/assets/image.png',
-            mediaStream: null,
-            imageData: {
-                image: '',
-                image_orientation: 0,
-            },
-        }
-    },
-    methods: {
-        captureImage() {
-            const mediaStreamTrack = this.mediaStream.getVideoTracks()[0]
-            const imageCapture = new window.ImageCapture(mediaStreamTrack)
-            let reader = new FileReader();
-            return imageCapture.takePhoto().then(blob => {
-                reader.readAsDataURL(blob)
-                reader.onload = () => {
-                    this.imageData.image = reader.result;
-                }
-            })  
-        },
-        rotateImage() {
-            this.imageData.image_orientation = this.imageData.image_orientation + 90; 
-        },
-        cancelImage() {
-            this.imageData.image = null;
-            this.showCameraModal = true;
-            navigator.mediaDevices.getUserMedia({video: true})
-            .then(mediaStream => {
-                    this.$refs.video.srcObject = mediaStream;
-                    this.$refs.video.play()
-                    this.mediaStream = mediaStream                   
-            }) 
-        },
-        uploadImage() {
-            axios({ method: "POST", "url": API_IMAGE_ENDPOINT, "data": this.imageData})
-                    .then(response => {
-                        this.response = response.data;    
-                     })
-        },
-
-    },
-    mounted() {
-        navigator.mediaDevices.getUserMedia({video: true})
-            .then(mediaStream => {
-                    this.$refs.video.srcObject = mediaStream;
-                    this.$refs.video.play()
-                    this.mediaStream = mediaStream                   
-            })   
-    },
-}
 </script>
 
 <style>
