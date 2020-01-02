4082e0c99f85cb3f1cd2c10fa96709b86dd9318b
jbull328 <jackbull328@gmail.com>
2020-01-02 10:09:32 -0800

    added master files.

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <CameraDisplay />
+  </div>
+</template>
+
+<script>
+import CameraDisplay from './components/CameraDisplay';
+
+export default {
+  name: 'app',
+  components: {
+    CameraDisplay
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
